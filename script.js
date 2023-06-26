7d3abe4c0776f2fd335eaa6a16f5aee8d85a893f
imrankabir02 <imrankabir325@gmail.com>
2023-06-27 04:21:27 +0600

    Email Validation issues resolved!

--- script.js
+++ script.js
@@ -1,36 +1,33 @@
-
-addEventListener("submit", (e) => {
-    e.preventDefault();
-    checkInput();
-
-});
-
 function checkInput(){
     const email = document.getElementById("email");
     const emailValue = email.value.trim();
     if(emailValue === ""){
         setErrorMessage(email, "Email Required!");
         email.focus();
-    }
-    else if(!isValidateEmail(emailValue)){
+
+    }else if(!isValidateEmail(emailValue)){
         setErrorMessage(email, "Email is not valid!");
         email.focus();
-    }
-    else{
-        setSuccessMessage(email, "Success!");
+        return 0;
+    }else{
+      setSuccessMessage(email, "Success!");
     }
 }
 
 function setErrorMessage(input, msg){
-    const formControl = input.parentElement;
-    const small = formControl.querySelector("small");
-    small.innerText = msg;
-    formControl.className = "form-control error";
+    const email = document.getElementById("msg");
+    email.style.display = "block";
+    email.innerText = msg;
 }
 
-function setSuccessMessage(input){
-    const formControl = input.parentElement;
-    formControl.className = "form-control success";
+function setSuccessMessage(input,msg){
+  const emailForm = document.getElementById("emailForm");
+  emailForm.submit();
+  const email = document.getElementById("msg");
+  email.classList.remove("alert-danger");
+  email.classList.add("alert-success");
+  email.style.display = "block";
+  email.innerText = msg;
 }
 
 function isValidateEmail (email){
@@ -38,31 +35,7 @@ function isValidateEmail (email){
     return pattern.test(email);
 }
 
-
-// // $('.email-input').on('blur', function() {
-// //     console.log("jjj")
-// //     var email = $(this).val(); // Get the value of the email input field
-// //     var emailRegex = /^[\w-]+(\.[\w-]+)*@([\w-]+\.)+[a-zA-Z]{2,7}$/; // Regular expression for email validation
-// //     if (!emailRegex.test(email)) {
-// //       // Invalid email address
-// //         $(this).addClass('invalid');
-// //         alert("No");
-// //     } else {
-// //       // Valid email address
-// //         $(this).removeClass('invalid');
-// //     }
-// // });
-// // const email = document.getElementsByClassName("email-input");
-
-// var emailInput = document.querySelector('email-input');
-// emailInput.addEventListener('blur', function() {
-//     var email = emailInput.value; // Get the value of the email input field
-//     var emailRegex = /^[\w-]+(\.[\w-]+)*@([\w-]+\.)+[a-zA-Z]{2,7}$/; // Regular expression for email validation
-//     if (!emailRegex.test(email)) {
-//     // Invalid email address
-//         emailInput.classList.add('invalid');
-//     } else {
-//       // Valid email address
-//         emailInput.classList.remove('invalid');
-//     }
-// });
+document.addEventListener('keydown', function(event) {
+  const msg = document.getElementById('msg');
+  msg.style.display = 'none';
+});
